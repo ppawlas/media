2bb67983a8c46afe931ad3639f56e08b92f8f287
ppawlas <ppawlas@gmail.com>
2017-01-28 00:51:59 +0100

    clean version

--- config/app.php
+++ config/app.php
@@ -166,6 +166,9 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         //
 
@@ -224,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
